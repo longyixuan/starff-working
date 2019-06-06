7409aec925b67f02d509e130f3922bfb5ceb5c96
yinxl <m18234089389@163.com>
2019-06-06 15:24:52 +0800

    添加代理

--- 前台2/vue.config.js
+++ 前台2/vue.config.js
@@ -1,8 +1,27 @@
+var os = require('os');
+var lacalhost = ''
+try {
+var network = os.networkInterfaces()
+localhost = network[Object.keys(network)[0]][1].address
+} catch (e) {
+localhost = 'localhost';
+}
 module.exports = {
     devServer: {
-        host: 'localhost',
+        host: lacalhost,
+        overlay: {
+            errors: true,
+        },
+        proxy: {
+            '/api': {
+                target: 'http://172.16.10.185:3333',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
         port: 9999
     },
-    // 打包时不生成.map文件 避免看到源码
     productionSourceMap: false
 }
