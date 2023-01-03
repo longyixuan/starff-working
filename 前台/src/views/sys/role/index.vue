9837d6e005479a062df13603995e4e0bd04c9237
yinxl <yinxl@chsi.com.cn>
2023-01-03 16:37:51 +0800

    任务管理

---
+++ 前台/src/views/sys/role/index.vue
@@ -0,0 +1,21 @@
+<style lang="less">
+@import './index.less';
+</style>
+
+<template>
+    <Card title="角色管理">
+    </Card>
+</template>
+<script>
+export default {
+    name: 'role',
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted() {
+    },
+};
+</script>
