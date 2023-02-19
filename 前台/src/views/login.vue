3a2505298283de3a649012bc188565d51de60f19
yinxl <yinxl@chsi.com.cn>
2023-02-19 18:22:02 +0800

    iview制定4.4.0，row高版本为flex布局，影响页面样式

--- 前台/src/views/login.vue
+++ 前台/src/views/login.vue
@@ -1,141 +1,117 @@
 <template>
-<div class="login">
-  <div class="login-warp">
-    <div class="login-left"></div>
-    <div class="login-right">
-      <Row
-        type="flex"
-        justify="center"
-        align="middle"
-        @keydown.enter.native="submitLogin"
-      >
-        <Col style="width: 368px;">
-          <Row class="header">
-            设计部工作管理系统
-          </Row>
-          <Alert type="error" show-icon v-if="error">{{errorMsg}}</Alert>
-          <Row class="login-form">
-            <Form ref="userNameLoginForm" :model="form" :rules="rules" class="form">
-              <FormItem prop="userName">
-                <Input
-                  v-model="form.userName"
-                  prefix="ios-contact"
-                  size="large"
-                  clearable
-                  placeholder="请输入用户名"
-                  autocomplete="off"
-                />
-              </FormItem>
-              <FormItem prop="password">
-                <Input
-                  type="password"
-                  v-model="form.password"
-                  prefix="ios-lock"
-                  size="large"
-                  clearable
-                  placeholder="请输入密码"
-                  autocomplete="off"
-                />
-              </FormItem>
-            </Form>
-            <Row type="flex" justify="space-between" class="code-row-bg">
-              <Dropdown>
-                <router-link to="/regist">
-                  <a class="forget-pass">注册账户</a>
-                </router-link>
-              </Dropdown>
-            </Row>
-            <Row>
-              <Button
-                class="login-btn"
-                type="primary"
-                size="large"
-                :loading="loading"
-                @click="submitLogin"
-                long
-              >
-                <span v-if="!loading">登录</span>
-                <span v-else>登录中...</span>
-              </Button>
-            </Row>
-          </Row>
-        </Col>
-      </Row>
+    <div class="login">
+        <div class="login-warp">
+            <div class="login-left"></div>
+            <div class="login-right">
+                <Row type="flex" justify="center" align="middle" @keydown.enter.native="submitLogin">
+                    <Col style="width: 368px;">
+                    <Row class="header">
+                        设计部工作管理系统
+                    </Row>
+                    <Alert type="error" show-icon v-if="error">{{ errorMsg }}</Alert>
+                    <Row class="login-form">
+                        <Form ref="userNameLoginForm" :model="form" :rules="rules" class="form">
+                            <FormItem prop="userName">
+                                <Input v-model="form.userName" prefix="ios-contact" size="large" clearable
+                                    placeholder="请输入用户名" autocomplete="off" />
+                            </FormItem>
+                            <FormItem prop="password">
+                                <Input type="password" v-model="form.password" prefix="ios-lock" size="large" clearable
+                                    placeholder="请输入密码" autocomplete="off" />
+                            </FormItem>
+                        </Form>
+                        <Row type="flex" justify="space-between" class="code-row-bg">
+                            <Dropdown>
+                                <router-link to="/regist">
+                                    <a class="forget-pass">注册账户</a>
+                                </router-link>
+                            </Dropdown>
+                        </Row>
+                        <Row>
+                            <Button class="login-btn" type="primary" size="large" :loading="loading"
+                                @click="submitLogin" long>
+                                <span v-if="!loading">登录</span>
+                                <span v-else>登录中...</span>
+                            </Button>
+                        </Row>
+                    </Row>
+                    </Col>
+                </Row>
+            </div>
+        </div>
     </div>
-    </div>
-  </div>
 </template>
 
 <script>
 import { login, userInfo, getJWT } from "@/api/index";
 export default {
-  data() {
-    return {
-      error: false,
-      errorMsg: "",
-      saveLogin: true,
-      loading: false,
-      form: {
-        userName: "",
-        password: ""
-      },
-      rules: {
-        userName: [
-          {
-            required: true,
-            message: "账号不能为空",
-            trigger: "blur"
-          }
-        ],
-        password: [
-          {
-            required: true,
-            message: "密码不能为空",
-            trigger: "blur"
-          }
-        ]
-      }
-    };
-  },
-  components: {
-  },
-  methods: {
-    showErrorMsg(msg) {
-      this.error = true;
-      this.errorMsg = msg;
-    },
-    submitLogin() {
-      this.$refs.userNameLoginForm.validate(valid => {
-        if (valid) {
-          this.loading = true;
-          login({
-            userName: this.form.userName,
-            password: this.form.password,
-            saveLogin: this.saveLogin
-          }).then(res => {
-            if (res.code === 1) {
-              this.setStore("accessToken", res.token);
-              // 获取用户信息
-              userInfo().then(res1 => {
-                if (res1.code === 1) {
-                  this.setStore("userInfo", res.data);
-                  this.$router.push({
-                    name: "home_index"
-                  });
-                } else {
-                  this.loading = false;
-                }
-              });
-            } else {
-              this.loading = false;
-              this.error = true;
-              this.errorMsg = res.msg;
+    data() {
+        return {
+            error: false,
+            errorMsg: "",
+            saveLogin: true,
+            loading: false,
+            form: {
+                userName: "",
+                password: ""
+            },
+            rules: {
+                userName: [
+                    {
+                        required: true,
+                        message: "账号不能为空",
+                        trigger: "blur"
+                    }
+                ],
+                password: [
+                    {
+                        required: true,
+                        message: "密码不能为空",
+                        trigger: "blur"
+                    }
+                ]
             }
-          });
+        };
+    },
+    components: {
+    },
+    methods: {
+        showErrorMsg(msg) {
+            this.error = true;
+            this.errorMsg = msg;
+        },
+        submitLogin() {
+            this.$refs.userNameLoginForm.validate(valid => {
+                if (valid) {
+                    this.loading = true;
+                    login({
+                        userName: this.form.userName,
+                        password: this.form.password,
+                        saveLogin: this.saveLogin
+                    }).then(res => {
+                        if (res.code === 1) {
+                            this.setStore("accessToken", res.token);
+                            // 获取用户信息
+                            userInfo().then(res1 => {
+                                if (res1.code === 1) {
+                                    this.setStore("userInfo", res.data);
+                                    this.$router.push({
+                                        name: "home_index"
+                                    });
+                                } else {
+                                    this.loading = false;
+                                }
+                            });
+                        } else {
+                            this.loading = false;
+                            this.error = true;
+                            this.errorMsg = res.msg;
+                        }
+                    });
+                }
+            });
         }
-      });
     }
-  }
 };
 </script>
 
--- 前台/src/views/regist-result.vue
+++ 前台/src/views/regist-result.vue
@@ -1,44 +1,44 @@
 <template>
-<div class="regist">
-  <div class="login-warp">
-    <div class="login-left"></div>
-    <div class="login-right">
-    <Row type="flex" justify="center" align="middle" @keydown.enter="submitRegist">
-        <Col :xs="{span:22}" style="width: 368px;">
-        <Row class="header">
-            设计部工作管理系统
-        </Row>
-    
-        <Row class="success">
-            <Icon type="md-checkmark-circle" color="#52c41a" size="64"></Icon>
-            <p class="success-words">恭喜您，您的账户：{{userName}} 注册成功</p>
-            <Row class="buttons">
-                <router-link to="/login">
-                    <Button type="primary" size="large" style="margin-right:10px;">立即登录</Button>
-                </router-link>
-                <router-link to="/regist">
-                    <Button size="large">返回注册</Button>
-                </router-link>
-            </Row>
-        </Row>
-        </Col>
-    </Row>
+    <div class="regist">
+        <div class="login-warp">
+            <div class="login-left"></div>
+            <div class="login-right">
+                <Row type="flex" justify="center" align="middle" @keydown.enter="submitRegist">
+                    <Col :xs="{ span: 22 }" style="width: 368px;">
+                    <Row class="header">
+                        设计部工作管理系统
+                    </Row>
+
+                    <Row class="success">
+                        <Icon type="md-checkmark-circle" color="#52c41a" size="64"></Icon>
+                        <p class="success-words">恭喜您，您的账户：{{ userName }} 注册成功</p>
+                        <Row class="buttons">
+                            <router-link to="/login">
+                                <Button type="primary" size="large" style="margin-right:10px;">立即登录</Button>
+                            </router-link>
+                            <router-link to="/regist">
+                                <Button size="large">返回注册</Button>
+                            </router-link>
+                        </Row>
+                    </Row>
+                    </Col>
+                </Row>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userName: ""
-    };
-  },
-  methods: {},
-  mounted() {
-    this.userName = this.$route.query.userName;
-  }
+    data() {
+        return {
+            userName: ""
+        };
+    },
+    methods: {},
+    mounted() {
+        this.userName = this.$route.query.userName;
+    }
 };
 </script>
 
